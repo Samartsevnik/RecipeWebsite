cc08055e19273738274cd514375672172aca48d5
samartsevnik <hukuta08@gmail.com>
2024-09-17 20:50:43 -0300

    build: add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import { Button } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,8 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img
-            src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
